b54a1b7e9e1dc2bebddb2cedd19cb7e8a87c4eb9
Sam Selvanathan <sjasel@gmail.com>
2014-12-29 16:41:57 -0800

    added jscs and jshint

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ var React = require('react-tools');
 
 module.exports = function(options) {
 
-    options.ext = options.ext || 'jsx';
+	options.ext = options.ext || 'jsx';
 
-	return function (data, args, callback) {
-		// react-tools throws :( 
-		// so try-catch the transformation
+	return function(data, args, callback) {
+		// react-tools throws :( so try-catch the transformation
 		try {
 			var transform = React.transform(data.toString('utf8'), {});
 			callback(null, transform);
